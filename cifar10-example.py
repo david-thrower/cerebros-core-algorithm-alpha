5ce9e59eed955109f81511e004338c84fdc6aa70
David Thrower <mcguyver07@gmail.com>
2023-02-09 23:35:22 -0500

    First attempt at cifar10 test.

--- cifar10-example.py
+++ cifar10-example.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import pandas as pd
 import numpy as np
+import imageio
 # from multiprocessing import Pool  # , Process
 from cerebros.simplecerebrosrandomsearch.simple_cerebros_random_search\
     import SimpleCerebrosRandomSearch
@@ -19,7 +20,34 @@ from ast import literal_eval
 
 # How many of the samples in the data set to actually use on this training run
 number_of_samples_to_use = 200
-INPUT_SHAPES = (224, 224, 3)
+INPUT_SHAPES = [(32, 32, 3)]  # resize from ]
+RESIZE_TO = (224, 224, 3)
+
+# Read in the data set and make it useable
+
+ciphar10_metadata = pd.read_csv("cifar10-mini/file_metadata.csv")
+
+ciphar10_train = ciphar10_metadata.query("data_set == 'train'")
+ciphar10_test = ciphar10_metadata.query("data_set == 'test'")
+
+
+def make_dataset(dataset):
+    images = []
+    labels = []
+    for i in np.arange(ciphar10_metadata.shape[0]):
+        imfile = ciphar10_metadata.loc[i]['file_name']
+        img = imageio.imread(imfile)
+        images.append(np.array(img))
+        labels.append(int(ciphar10_metadata.loc[i]['label']))
+    data_tensor = tf.constant(images)
+    labels_tensor = tf.constant(labels)
+    labels_tensor_ohe = tf.one_hot(labels_tensor)
+    return data_tensor, labels_tensor_ohe
+
+
+selected_x_train, selected_y_train_ohe =\
+    make_dataset(ciphar10_train)
+
 
 # Cerebros configurables:
 
@@ -39,8 +67,14 @@ maximum_neurons_per_unit = 4  # [2,20]
 
 # Build BERT base model
 image_input_0 = tf.keras.layers.Input(shape=INPUT_SHAPES[0])
+resized = tf.keras.layers.Resizing(
+    height=RESIZE_TO[0],
+    width=RESIZE_TO[1],
+    interpolation='bilinear',
+    crop_to_aspect_ratio=False)(image_input_0)
+
 preprocessor = hub.KerasLayer("https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4",
-                              output_shape=[1001])
+                              output_shape=[1001])(resized)
 classifier_output = preprocessor(image_input_0)
 foundation_model = tf.keras.Model(image_input_0,
                                   classifier_output)
@@ -60,12 +94,6 @@ embedding_model = tf.keras.Model(inputs=foundation_model.layers[0].input,
 # raw_text[prediction_target_column] =\
 #   raw_text[prediction_target_column]\
 #   .apply(lambda x: 1 if x == positive_class_label else 0)
-labels = ''  # raw_text.pop(prediction_target_column)
-labels = ''  # labels.values
-data = ''  # raw_text.values
-
-labels_tensor = tf.constant(labels, dtype=tf.int8)
-data_tensor = tf.constant(data, dtype=tf.string)
 
 TIME = pendulum.now(tz='America/New_York').__str__()[:16]\
     .replace('T', '_')\
@@ -76,10 +104,10 @@ INPUT_SHAPES = [()]
 
 # Cerebros parameters:
 
-training_x = [data_tensor]
-train_labels = [labels_tensor]
+training_x = [selected_x_train]
+train_labels = [selected_y_train_ohe]
 
-OUTPUT_SHAPES = [1]
+OUTPUT_SHAPES = [3]
 meta_trial_number = str(int(np.random.random() * 10 ** 12))
 
 cerebros_automl = SimpleCerebrosRandomSearch(
@@ -90,7 +118,7 @@ cerebros_automl = SimpleCerebrosRandomSearch(
     labels=train_labels,
     validation_split=0.35,
     direction='maximize',
-    metric_to_rank_by="val_binary_accuracy",
+    metric_to_rank_by="val_top_1_categorical_accuracy",
     minimum_levels=2,
     maximum_levels=maximum_levels,
     minimum_units_per_level=1,
@@ -98,7 +126,7 @@ cerebros_automl = SimpleCerebrosRandomSearch(
     minimum_neurons_per_unit=1,
     maximum_neurons_per_unit=maximum_neurons_per_unit,
     activation=activation,
-    final_activation='sigmoid',
+    final_activation='softmax',
     number_of_architecture_moities_to_try=2,
     number_of_tries_per_architecture_moity=1,
     minimum_skip_connection_depth=1,
@@ -116,17 +144,17 @@ cerebros_automl = SimpleCerebrosRandomSearch(
     p_lateral_connection_decay=zero_95_exp_decay,
     num_lateral_connection_tries_per_unit=num_lateral_connection_tries_per_unit,
     learning_rate=learning_rate,
-    loss=tf.keras.losses.BinaryCrossentropy(),
-    metrics=[tf.keras.metrics.BinaryAccuracy(),
-             tf.keras.metrics.Precision(),
-             tf.keras.metrics.Recall()],
+    loss=tf.keras.losses.CategoricalCrossentropy(),
+    metrics=[tf.keras.metrics.TopKCategoricalAccuracy(
+                k=1, name='top_1_categorical_accuracy')
+             ],
     epochs=epochs,
     project_name=f"{PROJECT_NAME}_meta_{meta_trial_number}",
     # use_multiprocessing_for_multiple_neural_networks=False,  # pull this param
     model_graphs='model_graphs',
     batch_size=batch_size,
     meta_trial_number=meta_trial_number,
-    base_models=[embedding_model],
-    train_data_dtype=tf.string)
-val_binary_accuracy = cerebros_automl.run_random_search()
+    base_models=[embedding_model])
+val_binary_accuracy =\
+    cerebros_automl.run_random_search()
 print(val_binary_accuracy)
