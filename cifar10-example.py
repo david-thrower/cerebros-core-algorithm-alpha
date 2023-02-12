60680eb457480a16fb25b5187cf9bf30415c29e0
David Thrower <mcguyver07@gmail.com>
2023-02-12 12:11:48 -0500

    Added required arg: depth to tf.one_hot.

--- cifar10-example.py
+++ cifar10-example.py
@@ -46,7 +46,8 @@ def make_dataset(dataset):
         labels.append(int(ciphar10_metadata.loc[i]['label']))
     data_tensor = tf.constant(images)
     labels_tensor = tf.constant(labels)
-    labels_tensor_ohe = tf.one_hot(labels_tensor)
+    labels_tensor_ohe = tf.one_hot(indices=labels_tensor,
+                                   depth=10)
     print(f"labels_tensor shape: {labels_tensor.shape}")
     print(f"data_tensor shape: {data_tensor.shape}")
     return data_tensor, labels_tensor_ohe
