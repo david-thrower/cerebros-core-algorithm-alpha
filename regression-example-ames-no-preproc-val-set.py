f0cf9962b635e803f025b3358242167baf3b6a57
David Thrower <mcguyver07@gmail.com>
2023-10-04 16:18:04 -0400

    Update regression-example-ames-no-preproc-val-set.py
    Debug XGB add missing module sklearn.model_selection. train_test_split.

--- regression-example-ames-no-preproc-val-set.py
+++ regression-example-ames-no-preproc-val-set.py
@@ -6,6 +6,7 @@ from cerebros.simplecerebrosrandomsearch.simple_cerebros_random_search\
     import SimpleCerebrosRandomSearch
 import pendulum
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import xgboost as xgb
 import tensorflow as tf
