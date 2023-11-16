c3eb87a20cbf467da5b8b77325d4135ba9ed5caf
David Thrower <mcguyver07@gmail.com>
2023-11-16 15:02:37 -0500

    Update text-class-ham-or-spam.py
    Revert to Bert model that was working (with jit_compile=False)

--- text-class-ham-or-spam.py
+++ text-class-ham-or-spam.py
@@ -49,25 +49,25 @@ text_input = tf.keras.layers.Input(shape=(), dtype=tf.string)
 
 
 # Old:
-# preprocessor = hub.KerasLayer(
-#     "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
-# encoder_inputs = preprocessor(text_input)
-# encoder = hub.KerasLayer(
-#     "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4",
-#     trainable=True)
+preprocessor = hub.KerasLayer(
+    "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
+encoder_inputs = preprocessor(text_input)
+encoder = hub.KerasLayer(
+    "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4",
+    trainable=True)
 
 # New:
-preprocessor =\
-    hub.KerasLayer(
-        "https://kaggle.com/models/tensorflow/"
-        "bert/frameworks/TensorFlow2/"
-        "variations/en-uncased-preprocess/versions/3")
-encoder_inputs = preprocessor(text_input)
-encoder =\
-    hub.KerasLayer(
-        "https://www.kaggle.com/models/tensorflow/"
-        "bert/frameworks/TensorFlow2/variations/"
-        "bert-en-uncased-l-10-h-768-a-12/versions/2")
+# preprocessor =\
+#     hub.KerasLayer(
+#         "https://kaggle.com/models/tensorflow/"
+#         "bert/frameworks/TensorFlow2/"
+#         "variations/en-uncased-preprocess/versions/3")
+# encoder_inputs = preprocessor(text_input)
+# encoder =\
+#     hub.KerasLayer(
+#         "https://www.kaggle.com/models/tensorflow/"
+#         "bert/frameworks/TensorFlow2/variations/"
+#         "bert-en-uncased-l-10-h-768-a-12/versions/2")
 outputs = encoder(encoder_inputs)
 pooled_output = outputs["pooled_output"]      # [batch_size, 768].
 sequence_output = outputs["sequence_output"]  # [batch_size, seq_length, 768].
