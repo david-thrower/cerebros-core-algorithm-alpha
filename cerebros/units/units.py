68aef4c751ebc1cd367b16934e157a744612332b
David Thrower <david.thrower@outlook.com>
2023-12-22 18:12:52 -0500

    Update units.py

--- cerebros/units/units.py
+++ cerebros/units/units.py
@@ -543,10 +543,10 @@ class DenseUnit(Unit,
                     self.n_neurons,
                     "softsign", # activation_0,
                     name=f"{self.name}_dns_{rn_5}")(merged_neural_network_layer_input)
-            scale_up = tf.constant([scale_factor for _ in np.arange()])
+            scale_up = tf.constant([scale_factor for _ in np.arange(dense_output.shape[-1])])
             self.neural_network_layer = tf.keras.layers.Multiply()(
                 [dense_output, 
-                 dense_output.shape[-1]])
+                 scale_up])
             # self.neural_network_layer
             # =\
             #        tf.keras.layers.LayerNormalization(axis=-2)(dense_output)
