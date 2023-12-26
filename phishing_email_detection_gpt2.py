618c74de1aecc39a1b0a422142f36904e059d9b8
David Thrower <david.thrower@outlook.com>
2023-12-26 17:22:35 -0500

    Update phishing_email_detection_gpt2.py

--- phishing_email_detection_gpt2.py
+++ phishing_email_detection_gpt2.py
@@ -152,7 +152,7 @@ embedded =\
         input_length=max_seq_length,
         mask_zero=True)(tokens)
 flattened = tf.keras.layers.Flatten()(embedded)
-dropout_embedded = tf.keras.layers.Dropout(0.85)(flattened)
+dropout_embedded = tf.keras.layers.Dropout(0.93)(flattened)
 dense = tf.keras.layers.Dense(max_seq_length, activation=None)(dropout_embedded)
 soft_scaled = IdentitySoftSign()(dense)
 
