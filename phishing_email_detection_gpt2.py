a7ddeb9273dca43d274c860a61252a59c4790954
David Thrower <david.thrower@outlook.com>
2023-12-27 00:27:05 -0500

    Update phishing_email_detection_gpt2.py

--- phishing_email_detection_gpt2.py
+++ phishing_email_detection_gpt2.py
@@ -156,7 +156,7 @@ print(f"VOCABULARY_SIZE: {VOCABULARY_SIZE}")
 # Best so far: original params
 
 
-activation = 'gelu'
+activation = "softsign" # 'gelu'
 predecessor_level_connection_affinity_factor_first = 49.9999
 predecessor_level_connection_affinity_factor_main = 0.31456
 max_consecutive_lateral_connections = 22
