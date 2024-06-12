cd6d6bde07d9cc8f99cfec3734ab4ad0031485b1
David Thrower <david.thrower@outlook.com>
2024-06-12 18:40:41 -0400

    Update simple_cerebros_random_search.py

--- cerebros/simplecerebrosrandomsearch/simple_cerebros_random_search.py
+++ cerebros/simplecerebrosrandomsearch/simple_cerebros_random_search.py
@@ -572,7 +572,7 @@ class SimpleCerebrosRandomSearch(DenseAutoMlStructuralComponent,
 
     def get_best_model(self):
         best_model_path = self.best_model_path
-        best_model = clone_model(tf.keras.models.load_model(best_model_path)
+        best_model = clone_model(tf.keras.models.load_model(best_model_path))
         # best_model = tf.keras.models.load_model(self.best_model_path)
         return best_model
 
