f1aaab15ee710a47b780b1d8cc4ff234fdbdc322
David Thrower <david.thrower@outlook.com>
2024-06-13 15:13:34 -0400

    Update simple_cerebros_random_search.py

--- cerebros/simplecerebrosrandomsearch/simple_cerebros_random_search.py
+++ cerebros/simplecerebrosrandomsearch/simple_cerebros_random_search.py
@@ -7,7 +7,7 @@ from cerebros.denseautomlstructuralcomponent.\
     import DenseAutoMlStructuralComponent, DenseLateralConnectivity, \
     zero_7_exp_decay, zero_95_exp_decay, simple_sigmoid
 from cerebros.units.units import Unit, InputUnit, FinalDenseUnit
-from custom.custom import TernaryDenseLayer, GPT2Layer
+from cerebros.custom.custom import TernaryDenseLayer, GPT2Layer
 from cerebros.neuralnetworkfuture.neural_network_future \
     import NeuralNetworkFuture, RealNeuronNeuralNetworkFuture
 # from cmdutil.cmdutil import run_command
