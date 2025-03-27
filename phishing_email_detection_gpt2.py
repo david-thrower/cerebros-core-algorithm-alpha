b800cf7664e342ccfccfdb6500ca34edea05df86
David Thrower <david.thrower@outlook.com>
2025-03-26 20:53:13 -0400

    Update phishing_email_detection_gpt2.py
    Restore the best model yet.

--- phishing_email_detection_gpt2.py
+++ phishing_email_detection_gpt2.py
@@ -85,7 +85,7 @@ OUTPUT_SHAPES = [1]
 
 """### A custom GPT2 encoder layer for text embedding"""
 
-""" un - string out
+
 class GPT2Layer(tf.keras.layers.Layer):
 
     def __init__(self, max_seq_length, **kwargs):
@@ -183,7 +183,6 @@ gpt_time_on_one_model_min =  (gpt_t1 - gpt_t0) / 60
 hy_df = pd.DataFrame(history.history)
 print(hy_df)
 
-""" # end un - string out
 
 ### Cerebros model:
 
@@ -274,8 +273,8 @@ num_lateral_connection_tries_per_unit = 10
 learning_rate = 0.0000511065
 epochs = 15  # [1, 100]
 batch_size = 20
-minimum_levels = 4
-maximum_levels = 4 # [3,7]
+minimum_levels = 2
+maximum_levels = 3 # [3,7]
 
 minimum_units_per_level = 4
 maximum_units_per_level = 8
@@ -353,8 +352,7 @@ models_tried = moities_to_try  * tries_per_moity
 cerebros_time_per_model = cerebros_time_all_models_min / models_tried
 
 print(f"Cerebros trained {models_tried} models FROM A COLD START in ONLY {cerebros_time_all_models_min} min. Cerebros took only {cerebros_time_per_model} minutes on average per model.")
-# Un-comment out the next line
-# print(f"GPT2 took {gpt_time_on_one_model_min} just to FINE TUNE one PRE - TRAINED model. Although this is a small scale test, this shows the advantage of scaling in ON timing VS ON**2 timing.")
+print(f"GPT2 took {gpt_time_on_one_model_min} just to FINE TUNE one PRE - TRAINED model. Although this is a small scale test, this shows the advantage of scaling in ON timing VS ON**2 timing.")
 
 
 print(f'Cerebros best accuracy achieved is {result}')
