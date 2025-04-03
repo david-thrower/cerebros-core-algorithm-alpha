689f00394751ece3efeb0ab2d470576deed99614
David Thrower <david.thrower@outlook.com>
2025-04-03 15:44:11 -0400

    Update simple_cerebros_random_search.py
    Try to deal with progress bar floating away.

--- cerebros/simplecerebrosrandomsearch/simple_cerebros_random_search.py
+++ cerebros/simplecerebrosrandomsearch/simple_cerebros_random_search.py
@@ -523,6 +523,7 @@ class SimpleCerebrosRandomSearch(DenseAutoMlStructuralComponent,
         processes = []
         for i in tqdm(np.arange(self.number_of_architecture_moities_to_try),
                       desc=f"Moiety {iter_trial} of {self.number_of_architecture_moities_to_try} running",
+                      ascii=True,
                       colour="#16ceeb"):
             self.parse_neural_network_structural_spec_random()
             spec = self.get_neural_network_spec()
