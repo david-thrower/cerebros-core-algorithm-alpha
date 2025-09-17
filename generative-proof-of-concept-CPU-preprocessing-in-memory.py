39f7b7b32bf3ab8ac898ea06acaee01e2b54d840
David Thrower <david.thrower@outlook.com>
2025-09-17 01:29:58 -0400

    Update generative-proof-of-concept-CPU-preprocessing-in-memory.py
    Un-comment out model variable...

--- generative-proof-of-concept-CPU-preprocessing-in-memory.py
+++ generative-proof-of-concept-CPU-preprocessing-in-memory.py
@@ -690,7 +690,7 @@ def objective(trial: optuna.Trial) -> float:
     
     # MODEL_FILE_NAME = "cerebros-foundation-model.keras"
     
-    # best_model_found = cerebros_automl.get_best_model()
+    best_model_found = cerebros_automl.get_best_model()
     # best_model_found.save(MODEL_FILE_NAME)
     # del(best_model_found)
     # del(cerebros_automl)
