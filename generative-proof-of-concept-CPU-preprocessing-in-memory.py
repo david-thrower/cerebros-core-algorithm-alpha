394ec5b6d382a2be2aaaf845afa5c1a55413c229
David Thrower <david.thrower@outlook.com>
2025-09-18 01:55:21 -0400

    Update generative-proof-of-concept-CPU-preprocessing-in-memory.py
    add "as run" to with mlflow ...

--- generative-proof-of-concept-CPU-preprocessing-in-memory.py
+++ generative-proof-of-concept-CPU-preprocessing-in-memory.py
@@ -171,7 +171,7 @@ def objective(trial: optuna.Trial) -> float:
    
     tags = {"phase": "poc", "script": os.path.basename(__file__), "trial_number": str(trial.number), "Start_time": str(trial_start_time)}
 
-    with mlflow.start_run(run_name=run_name, tags=tags):
+    with mlflow.start_run(run_name=run_name, tags=tags) as run:
         # Log the hyperparameters
         mlflow.log_params(params)
 
