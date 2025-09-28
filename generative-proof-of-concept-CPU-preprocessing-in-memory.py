f3ce018b43d071cbf318172893ea809994a2fe41
David Thrower <david.thrower@outlook.com>
2025-09-27 20:22:28 -0400

    Update generative-proof-of-concept-CPU-preprocessing-in-memory.py
    Correct number of samples in actual run.

--- generative-proof-of-concept-CPU-preprocessing-in-memory.py
+++ generative-proof-of-concept-CPU-preprocessing-in-memory.py
@@ -63,7 +63,7 @@ def objective(trial: optuna.Trial) -> float:
     # Number of text samples to create: # Number of text samples (of approximately max_seq_len) to create 
     # Raises RAM in a linear fashion
     
-    SAMPLES_TO_CREATE = 75
+    SAMPLES_TO_CREATE = 1000
 
     # How many tokens to provide before expecting the next token to be predicted. 
     # Half this = double RAM  (inversely proportional to RAM requirement)
