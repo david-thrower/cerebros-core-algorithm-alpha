c51fd8bb74d2f6bf117fe8a0dbe53b27d5283931
David Thrower <david.thrower@outlook.com>
2025-10-12 23:07:46 -0400

    Update regression-example-ames-no-preproc-val-set.py
    Add a capital letter in the project name to test the bug fix.

--- regression-example-ames-no-preproc-val-set.py
+++ regression-example-ames-no-preproc-val-set.py
@@ -26,7 +26,7 @@ TIME = pendulum.now().__str__()[:16]\
     .replace('T', '_')\
     .replace(':', '_')\
     .replace('-', '_')
-PROJECT_NAME = f'{TIME}_cerebros_auto_ml_test'
+PROJECT_NAME = f'{TIME}_Cerebros_auto_ml_test'
 PROJECT_NAME = f"{PROJECT_NAME}-meta-{META_TRIAL_NUMBER}"
 
 def hash_a_row(row):
