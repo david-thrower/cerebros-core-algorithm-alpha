bfa5f0f3424f796aa8d458ab6652c01ba4ab5d26
David Thrower <david.thrower@outlook.com>
2025-11-19 20:26:36 -0500

    Enable callbacks in training function
    Add back early stopping after optimizing the LLM script on Stage I-b parameters.

--- train_a_generative_llm.py
+++ train_a_generative_llm.py
@@ -773,7 +773,7 @@ phase_i_b_history = \
         epochs=phase_i_b_epochs,
         # steps_per_epoch=train_steps,
         # validation_steps=val_steps # ,
-        # callbacks=callbacks_list
+        callbacks=callbacks_list
     )
 
 phase_i_b_history = \
