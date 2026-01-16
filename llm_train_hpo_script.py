7da03dcd7a683d32a42cbcb84d4a465004daf394
David Thrower <david.thrower@outlook.com>
2026-01-16 14:44:58 -0500

    Change moities_to_try from 2 to 1
    Reduce duplication of effort running multiple models that perform about the same...

--- llm_train_hpo_script.py
+++ llm_train_hpo_script.py
@@ -173,7 +173,7 @@ def objective(trial):
 
     PROJECTION_N = 1
 
-    moities_to_try = 2
+    moities_to_try = 1
     tries_per_moity = 1
 
     # --- Assemble the final params dictionary for logging ---
